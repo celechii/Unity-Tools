e46f4e85205a6626b1f25b981bb6fb50a2fd6d4e
Noé <kathryncharron@cmail.carleton.ca>
2019-06-26 17:27:59 -0400

    added speedrun clock

---
+++ SpeedrunClock.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SpeedrunClock : MonoBehaviour {
+
+	public double time;
+
+	private bool timeAdvance = true;
+
+	private void Update() {
+		if (timeAdvance)
+			time += (double)Time.unscaledDeltaTime;
+	}
+
+	public void StopTimer() {
+		timeAdvance = false;
+	}
+
+	public void ResumeTimer() {
+		timeAdvance = true;
+	}
+
+	[ContextMenu("Print Time")]
+	public string GetStringTime() {
+		string ret = System.TimeSpan.FromSeconds(time).ToString("hh\\:mm\\:ss\\.fff");
+
+		print(ret);
+		return ret;
+	}
+}
