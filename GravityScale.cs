f5f5195fb1e489b7103ad9b1129f1050d3b6262b
Noé <kathryncharron@cmail.carleton.ca>
2019-08-05 15:28:08 -0400

    added gravity scale for ur fucking ridigbodies

---
+++ GravityScale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityScale : MonoBehaviour {
+
+	public float gravityScale = 1;
+
+	private Rigidbody rb;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		rb.AddForce(Vector3.down * (-gravityScale + 1) * Physics.gravity.y * rb.mass);
+	}
+
+}
