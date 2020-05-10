2a27d7e060c86e6a4afb057c328e3f9ac8f36b40
Noé <kathryncharron@cmail.carleton.ca>
2020-05-10 18:06:33 -0400

    added summaries to all the functions

--- InputManager.cs
+++ InputManager.cs
@@ -19,11 +19,11 @@ public class InputManager : MonoBehaviour {
 		BuildLookup();
 	}
 
-	private void Update() {
-		if (GetKeyDown("test"))
-			print("boop");
-	}
-
+	/// <summary>
+	/// Has the key been pressed this frame?
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
 	public bool GetKeyDown(string name, bool negative = false) {
 		CheckNameValid(name);
 		InputAction action = lookup[name];
@@ -31,6 +31,11 @@ public class InputManager : MonoBehaviour {
 		return Input.GetKeyDown(negative?action.modifiedNegative : action.modifiedPositive);
 	}
 
+	/// <summary>
+	/// Is the key being pressed right now?
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
 	public bool GetKey(string name, bool negative = false) {
 		CheckNameValid(name);
 		InputAction action = lookup[name];
@@ -38,6 +43,11 @@ public class InputManager : MonoBehaviour {
 		return Input.GetKey(negative?action.modifiedNegative : action.modifiedPositive);
 	}
 
+	/// <summary>
+	/// Has the key been released this frame?
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
 	public bool GetKeyUp(string name, bool negative = false) {
 		CheckNameValid(name);
 		InputAction action = lookup[name];
@@ -45,15 +55,49 @@ public class InputManager : MonoBehaviour {
 		return Input.GetKeyUp(negative?action.modifiedNegative : action.modifiedPositive);
 	}
 
+	/// <summary>
+	/// Get the keycode of the input.
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
 	public KeyCode GetBinding(string name, bool negative = false) => negative ? lookup[name].modifiedNegative : lookup[name].modifiedPositive;
 
+	/// <summary>
+	/// Returns -1 if the negative key is pressed, 1 if the positive key is pressed, and 0 if both or neither.
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
 	public int GetAxis(string name) => (GetKey(name) ? 1 : 0) + (GetKey(name, true) ? 1 : 0);
 
+	/// <summary>
+	/// Has either of the keys on the axis been pressed this frame?
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
 	public bool GetAxisDown(string name) => GetKeyDown(name) || GetKeyDown(name, true);
 
+	/// <summary>
+	/// Has either of the keys on the axis been released this frame?
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	public bool GetAxisUp(string name) => GetKeyUp(name) || GetKeyUp(name, true);
+
+	/// <summary>
+	/// Sets the bindings of all keys back to their defaults.
+	/// </summary>
 	public void ResetAllBindings() {
 		for (int i = 0; i < keyBinds.actions.Length; i++)
 			keyBinds.actions[i].ResetBindings();
+		SaveKeyBinds();
+	}
+
+	/// <summary>
+	/// Resets a single input binding.
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
+	public void ResetBinding(string name, bool negative = false) {
+		CheckNameValid(name);
+
+		lookup[name].ResetBinding(negative);
 		SaveKeyBinds();
 	}
 
@@ -62,20 +106,20 @@ public class InputManager : MonoBehaviour {
 			throw new System.Exception($"hmm fuck, dont seem to have any binding w the name {name}???");
 	}
 
-	public void ResetBinding(string name, bool negative = false) {
-		CheckNameValid(name);
-
-		lookup[name].ResetBinding(negative);
-		SaveKeyBinds();
-	}
-
 	// to b used w SaveSystem.cs on github:
 	// https://github.com/celechii/Unity-Tools/blob/7d1182ea0f34b9b517bdbd67f88cb275316f1c2e/SaveSystem.cs
 	// <3
+	/// <summary>
+	/// Uses the save system to save the keybind JSON to /Resources/Saves/Keybinds.txt
+	/// </summary>
 	public void SaveKeyBinds() {
 		SaveSystem.SaveTxt("Keybinds", keyBinds);
 	}
 
+	/// <summary>
+	/// Uses the save system to load the keybind JSON from /Resources/Saves/Keybinds.txt
+	/// If nothing is found, it will load the defaults instead.
+	/// </summary>
 	public void LoadKeyBinds() {
 		if (SaveSystem.SaveExists("Keybinds"))
 			keyBinds = SaveSystem.LoadTxt<Keybinds>("Keybinds");
@@ -94,6 +138,12 @@ public class InputManager : MonoBehaviour {
 			lookup.Add(keyBinds.actions[i].name, keyBinds.actions[i]);
 	}
 
+	/// <summary>
+	/// Start the rebind process for a single input.
+	/// </summary>
+	/// <param name="name">The name of the input.</param>
+	/// <param name="callback">Method to call when the rebinding is complete.</param>
+	/// <param name="negative">Should access the negative value? Default is positive.</param>
 	public void RebindKey(string name, System.Action callback = null, bool negative = false) {
 		CheckNameValid(name);
 		StartCoroutine(RebindInput(name, callback, negative));
