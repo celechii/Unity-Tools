71522b48ef02abf29f3b61765284c04e57a70148
Noé <kathryncharron@cmail.carleton.ca>
2020-11-09 00:45:05 -0500

    FIXED THE FUCKING ANGLES

--- FuckingAngles.cs
+++ FuckingAngles.cs
@@ -1,13 +1,80 @@
 using UnityEngine;
 
-public abstract class FuckingAngles {
+public static class FuckingAngles {
+
+	/// <summary>
+	/// Converts any angle to an unsigned angle.
+	/// (Range of 0º to 359º) 
+	/// </summary>
+	/// <param name="angle">Signed angle.</param>
+	public static float Angle(float angle) {
+		angle = angle % 360f;
+		if (angle < 0)
+			angle = 360 - angle;
+		return angle;
+	}
+
+	/// <summary>
+	/// Adds degrees to an angle accounting for wrapping over 360º and under 0º
+	/// </summary>
+	/// <param name="angle">The initial angle.</param>
+	/// <param name="amount">Degrees to add.</param>
+	public static float AddToAngle(float angle, float amount) {
+		return Angle(angle + amount);
+	}
 
 	/// <summary>
 	/// Returns a normalized Vector2 direction.
 	/// </summary>
 	/// <param name="angleInDeg">Angle in degrees.</param>
-	public static Vector2 DirectionFromAngle(float angleInDeg) {
+	public static Vector2 DirFromAngle(float angleInDeg, float offset = 0) {
+		if (offset != 0)
+			angleInDeg = AddToAngle(angleInDeg, offset);
 		return new Vector2(Mathf.Cos(angleInDeg * Mathf.Deg2Rad), Mathf.Sin(angleInDeg * Mathf.Deg2Rad));
+	}
+
+	/// <summary>
+	/// Returns the angle of the vector direction.
+	/// (Range of 0º to 359º)
+	/// </summary>
+	/// <param name="dir">The direction.</param>
+	/// <param name="offset">Offset angle.</param>
+	public static float AngleFromDir(Vector2 dir, float offset = 0) {
+		return Angle(Vector2.SignedAngle(Vector2.right, dir) + offset);
+	}
+
+	/// <summary>
+	/// Returns the angle between two vector positions.
+	/// (Range of 0º to 359º)
+	/// </summary>
+	/// <param name="a">The start position.</param>
+	/// <param name="b">The end position.</param>
+	/// <param name="offset">Offset angle.</param>
+	public static float AngleBetweenPoints(Vector2 a, Vector2 b, float offset = 0) {
+		Vector2 difference = b - a;
+		return AngleFromDir(difference, offset);
+	}
+
+	/// <summary>
+	/// Converts any angle to a signed angle.
+	/// (Range of 180º to -179º)
+	/// </summary>
+	/// <returns>The signed angle.</returns>
+	/// <param name="angle">Angle.</param>
+	public static float SignedAngle(float angle) {
+		angle = Angle(angle);
+		if (angle > 180)
+			angle -= 180;
+		return angle;
+	}
+
+	/// <summary>
+	/// Adds degrees to an angle accounting for wrapping over 180º and under -179º
+	/// </summary>
+	/// <param name="angle">The initial angle.</param>
+	/// <param name="amount">Degrees to add.</param>
+	public static float AddToSignedAngle(float angle, float amount) {
+		return SignedAngle(angle + amount);
 	}
 
 	/// <summary>
@@ -15,8 +82,8 @@ public abstract class FuckingAngles {
 	/// (Range of -180º to 180º)
 	/// </summary>
 	/// <param name="direction">The direction.</param>
-	public static float SignedAngleBetweenV2(Vector2 direction) {
-		return SignedAngleBetweenV2(Vector2.zero, direction);
+	public static float SignedAngleFromDir(Vector2 direction, float offset = 0) {
+		return SignedAngle(AngleFromDir(direction, offset));
 	}
 
 	/// <summary>
@@ -25,90 +92,11 @@ public abstract class FuckingAngles {
 	/// </summary>
 	/// <param name="a">The start position.</param>
 	/// <param name="b">The end position.</param>
-	public static float SignedAngleBetweenV2(Vector2 a, Vector2 b) {
+	public static float SignedAngleBetweenPoints(Vector2 a, Vector2 b) {
 		Vector2 diference = b - a;
-		float sign = (b.y < a.y) ? -1.0f : 1.0f;
-		return Vector2.Angle(Vector2.right, diference) * sign;
+		return SignedAngleFromDir(diference);
 	}
 
-	/// <summary>
-	/// Converts an unsigned angle to a signed angle.
-	/// </summary>
-	/// <returns>The signed angle.</returns>
-	/// <param name="angle">Angle.</param>
-	public static float GetSignedAngle(float angle) {
-		while (angle > 180)
-			angle -= 360;
-		while (angle < -180)
-			angle += 360;
-
-		return angle;
-	}
-
-	/// <summary>
-	/// Converts an signed angle to an unsigned angle.
-	/// </summary>
-	/// <param name="signedAngle">Signed angle.</param>
-	public static float GetNormalAngle(float signedAngle) {
-		signedAngle = GetSignedAngle(signedAngle);
-		if (signedAngle < 0)
-			signedAngle += 360;
-		return signedAngle;
-	}
-
-	/// <summary>
-	/// Returns the angle of the vector direction.
-	/// (Range of 0º to 359º)
-	/// </summary>
-	/// <param name="direction">The direction.</param>
-	public static float AngleBetweenV2(Vector2 direction) {
-		return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-	}
-
-	/// <summary>
-	/// Returns the angle of the vector direction.
-	/// (Range of 0º to 359º)
-	/// </summary>
-	/// <param name="direction">The direction.</param>
-	/// <param name="offset">Offset angle.</param>
-	public static float AngleBetweenV2(Vector2 direction, float offset) {
-		return AddToAngle(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, offset);
-	}
-
-	/// <summary>
-	/// Returns the angle between two vector positions.
-	/// (Range of 0º to 359º)
-	/// </summary>
-	/// <param name="a">The start position.</param>
-	/// <param name="b">The end position.</param>
-	public static float AngleBetweenV2(Vector2 a, Vector2 b) {
-		Vector2 direction = b - a;
-		return AngleBetweenV2(direction);
-	}
-
-	/// <summary>
-	/// Returns the angle between two vector positions.
-	/// (Range of 0º to 359º)
-	/// </summary>
-	/// <param name="a">The start position.</param>
-	/// <param name="b">The end position.</param>
-	public static float AngleBetweenV2(Vector2 a, Vector2 b, float offset) {
-		Vector2 direction = b - a;
-		return AngleBetweenV2(direction, offset);
-	}
-	
-	/// <summary>
-	/// Adds degrees to an angle accounting for wrapping over 360º and under 0º
-	/// </summary>
-	/// <param name="angle">The initial angle.</param>
-	/// <param name="amount">Degrees to add.</param>
-	public static float AddToAngle(float angle, float amount) {
-		angle = (amount + angle) % 360f;
-		if (angle < 0)
-			angle = 360f + angle;
-		return angle;
-	}
-	
 	/// <summary>
 	/// Returns a perpendicular vector to the vector provided.
 	/// </summary>
@@ -116,4 +104,31 @@ public abstract class FuckingAngles {
 	/// <param name="direction">Negative value for left, positive value for right.</param>
 	public static Vector2 GetPerpendicular(Vector2 dir, int direction = -1) => new Vector2(dir.y * Mathf.Sign(direction), dir.x * -Mathf.Sign(direction));
 
+	/// <summary>
+	/// Rotates a Transform to point the right side towards the position of a target Transform.
+	/// </summary>
+	/// <param name="trans">The transform to be rotated.</param>
+	/// <param name="target">The target position to be pointing towards.</param>
+	/// <param name="maxDegrees">The maximum degrees to rotate towards.</param>
+	/// <param name="offset">Degrees to offset transform forward. 0 = right, 90 = up, 180 = left, 270 = down</param>
+	/// <param name="selfWorldSpace">Use the ref transform's worldspace position?</param>
+	/// <param name="targetWorldSpace">Use the target transform's worldspace position?</param>
+	public static void RotateTowards(ref Transform trans, Transform target, float maxDegrees = Mathf.Infinity, float offset = 0, bool selfWorldSpace = false, bool targetWorldSpace = false) {
+		RotateTowards(ref trans, targetWorldSpace ? target.position : target.localPosition, offset, maxDegrees, selfWorldSpace);
+	}
+
+	/// <summary>
+	/// Rotates a Transform to point the right side towards a target position.
+	/// </summary>
+	/// <param name="trans">The transform to be rotated.</param>
+	/// <param name="target">The target position to be pointing towards.</param>
+	/// <param name="maxDegrees">The maximum degrees to rotate towards.</param>
+	/// <param name="offset">Degrees to offset transform forward. 0 = right, 90 = up, 180 = left, 270 = down</param>
+	/// <param name="worldSpace">Use the transform's worldspace position?</param>
+	public static void RotateTowards(ref Transform trans, Vector2 target, float maxDegrees = Mathf.Infinity, float offset = 0, bool worldSpace = false) {
+		Vector2 selfPos = worldSpace ? trans.position : trans.localPosition;
+		Vector2 currentDir = DirFromAngle(worldSpace ? trans.eulerAngles.z : trans.localEulerAngles.z, offset);
+		Vector2 targetDir = target - selfPos;
+		trans.Rotate(0, 0, Mathf.Clamp(Vector2.SignedAngle(currentDir, targetDir), -maxDegrees, maxDegrees), Space.Self);
+	}
 }
