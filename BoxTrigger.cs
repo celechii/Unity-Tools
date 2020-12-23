1414ca9e231de635ab0967ff85c709832c4471eb
Noé <kathryncharron@cmail.carleton.ca>
2020-12-23 17:16:14 -0500

    boxtrigger n cooldown actions from undead gfs

---
+++ BoxTrigger.cs
@@ -0,0 +1,103 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BoxTrigger : MonoBehaviour {
+
+	public new bool enabled = true;
+	public bool visualize = true;
+	[Tag]
+	public string[] tags = new string[] { "Player" };
+	[Space]
+	public Events[] events = new Events[1];
+
+	private BoxCollider2D boxCollider;
+
+	private void Awake() {
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		CheckTrigger(other, TInteraction.Enter);
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+		CheckTrigger(other, TInteraction.Exit);
+	}
+
+	private void CheckTrigger(Collider2D other, TInteraction interaction) {
+		if (!enabled)
+			return;
+
+		foreach (string s in tags) {
+			if (other.CompareTag(s)) {
+				for (int i = 0; i < events.Length; i++) {
+					if (events[i].interactions.Has(interaction)) {
+						if (events[i].directions.Has(FindDirection(other.transform.localPosition)))
+							events[i].Call.Invoke();
+					}
+				}
+				return;
+			}
+		}
+	}
+
+	private TDirection FindDirection(Vector2 pos) {
+		TDirection dir = TDirection.Top;
+		float smallestDist = Mathf.Abs(pos.y - boxCollider.bounds.max.y);
+
+		float temp = Mathf.Abs(boxCollider.bounds.min.y - pos.y);
+		if (temp < smallestDist) {
+			dir = TDirection.Bottom;
+			smallestDist = temp;
+		}
+		temp = Mathf.Abs(boxCollider.bounds.min.x - pos.x);
+		if (temp < smallestDist) {
+			dir = TDirection.Left;
+			smallestDist = temp;
+		}
+		temp = Mathf.Abs(boxCollider.bounds.max.x - pos.x);
+		if (temp < smallestDist)
+			dir = TDirection.Right;
+
+		return dir;
+	}
+
+	[Button]
+	private void SetTransformFromBoxOffset() {
+		transform.Translate(boxCollider.offset);
+		boxCollider.offset = Vector2.zero;
+	}
+
+	private void OnDrawGizmos() {
+		if (visualize) {
+			Gizmos.color = new Color(enabled ? 0 : 1, enabled ? 1 : 0, 0, .3f);
+			if (boxCollider == null)
+				boxCollider = GetComponent<BoxCollider2D>();
+			Gizmos.DrawCube(boxCollider.offset + (Vector2)transform.localPosition, boxCollider.size);
+		}
+	}
+
+	[System.Serializable]
+	public class Events {
+		public TInteraction interactions = (TInteraction)(-1);
+		[MultiEnum]
+		public TDirection directions = (TDirection)(-1);
+		public UnityEvent Call;
+	}
+
+	[System.Flags]
+	public enum TInteraction {
+		Enter = 1 << 0,
+		Exit = 1 << 1
+	}
+
+	[System.Flags]
+	public enum TDirection {
+		Top = 1 << 0,
+		Bottom = 1 << 1,
+		Left = 1 << 2,
+		Right = 1 << 3
+	}
+}
---
+++ CooldownAction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class CooldownAction {
+
+	/// <summary>
+	/// No cooldown at 0, full cooldown at 1.
+	/// </summary>
+	public float percentRemaining => cooldownLeft / cooldownDuration;
+
+	private Action action;
+	public float cooldownDuration;
+	public float cooldownLeft { get; private set; }
+	private float bufferTime;
+	private float bufferLeft;
+
+	/// <param name="action">The action you want called.</param>
+	/// <param name="cooldownDuration">How long to cooldown between action calls.</param>
+	/// <param name="bufferTime">How long to hold onto an Invoke before losing it to the void.</param>
+	public CooldownAction(float cooldownDuration, Action action = null, float bufferTime = 0) {
+		if (action != null)
+			this.action += action;
+		this.cooldownDuration = cooldownDuration;
+		this.bufferTime = bufferTime;
+	}
+
+	public void AddAction(Action action) => this.action += action;
+	public void RemoveAction(Action action) => this.action -= action;
+
+	/// <summary>
+	/// Updates the timers with deltaTime.
+	/// Might also call the action if its been buffered.
+	/// </summary>
+	public void UpdateTime(float deltaTime) {
+		if (cooldownLeft > 0)
+			cooldownLeft -= Math.Min(deltaTime, cooldownLeft);
+
+		if (bufferLeft > 0) {
+			bufferLeft -= Math.Min(deltaTime, bufferLeft);
+			if (bufferLeft > 0 && cooldownLeft == 0)
+				Invoke();
+		}
+	}
+
+	/// <summary>
+	/// Calls the action, returns true if the action can be called taking the buffer time into account.
+	/// </summary>
+	public bool Invoke() {
+		if (action == null)
+			return false;
+
+		if (cooldownLeft <= 0) {
+			action.Invoke();
+			bufferLeft = 0;
+			cooldownLeft = cooldownDuration;
+			return true;
+		} else {
+			bufferLeft = bufferTime;
+			return cooldownLeft <= bufferTime;
+		}
+	}
+}
