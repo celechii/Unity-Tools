25c5f6d0525a735cd5ea1049579f382a37b411f9
Noé <kathryncharron@cmail.carleton.ca>
2020-12-31 18:57:21 -0500

    new multienum tools

--- MultiEnum/MultiEnumAttribute.cs
+++ MultiEnum/MultiEnumAttribute.cs
@@ -1,15 +1,23 @@
-﻿// #if UNITY_EDITOR
-using UnityEngine;
+﻿using UnityEngine;
 
 // by froodydude
 // https://answers.unity.com/questions/486694/default-editor-enum-as-flags-.html?childToView=514102#answer-514102
 
 // how to use cause ik you'll forget:
-// [System.Flags] enum Weekday { Su = 1, M = 2, Tu = 4, W = 8, Th = 16, F = 32, Sa = 64}
-// then
-// [MultiEnum] public Weekday day;
+// [System.Flags]
+// enum Weekday {
+// 	Su = 1 << 0,
+// 	M = 1 << 1,
+// 	Tu = 1 << 2,
+// 	W = 1 << 3,
+// 	Th = 1 << 4,
+// 	F = 1 << 5,
+// 	Sa = 1 << 6
+// }
+
+// [MultiEnum]
+// public Weekday day;
 
 public class MultiEnumAttribute : PropertyAttribute {
 	public MultiEnumAttribute() {}
 }
-// #endif
--- MultiEnum/MultiEnumHelper.cs
+++ MultiEnum/MultiEnumHelper.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 
 public static class MultiEnumHelper {
 
+	/// <summary>
+	/// Does the enum contain the flag?
+	/// </summary>
 	public static bool Has<TEnum>(this TEnum multiEnum, TEnum flag)where TEnum : struct, IConvertible {
-		int flagVal = Convert.ToInt32(flag);
-		if (flagVal == 0)
-			throw new ArgumentException("hey wtf don't pass in a flag with a value of 0 >:(");
-
-		return (Convert.ToInt32(multiEnum) & flagVal) > 0;
+		return (Convert.ToInt32(multiEnum) & Convert.ToInt32(flag)) > 0;
 	}
 
+	/// <summary>
+	/// Does this enum have each flag?
+	/// </summary>
 	public static bool HasNone<TEnum>(this TEnum multiEnum)where TEnum : struct, IConvertible => Convert.ToInt32(multiEnum) == 0;
 
+	/// <summary>
+	/// Does this enum have each flag?
+	/// </summary>
+	public static bool HasAll<TEnum>(this TEnum multiEnum, TEnum flag)where TEnum : struct, IConvertible {
+		return multiEnum.Has<TEnum>(flag);
+	}
+
+	/// <summary>
+	/// Does this enum have each of the flags set?
+	/// </summary>
+	public static bool HasAll<TEnum>(this TEnum multiEnum, params TEnum[] flags)where TEnum : struct, IConvertible {
+		foreach (TEnum flag in flags)
+			if (!multiEnum.Has(flag))
+				return false;
+		return true;
+	}
+
+	/// <summary>
+	/// Does this enum have every flag set?
+	/// </summary>
 	public static bool HasAll<TEnum>(this TEnum multiEnum)where TEnum : struct, IConvertible {
 		foreach (TEnum flag in Enum.GetValues(typeof(TEnum)))
 			if (!multiEnum.Has(flag))
@@ -21,19 +43,22 @@ public static class MultiEnumHelper {
 	}
 
 	/// <summary>
-	/// sets all the flags
+	/// Gets a combined enum with all the specified flags set
 	/// </summary>
-	public static void SetAll<TEnum>(ref this TEnum multiEnum)where TEnum : struct, IConvertible {
-		int value = 0;
-		int enumSize = Enum.GetValues(typeof(TEnum)).Length;
-		for (int i = 0; i < enumSize; i++)
-			value += 1 << i;
+	public static TEnum Combine<TEnum>(params TEnum[] flags)where TEnum : struct, IConvertible {
+		if (flags.Length == 0)
+			throw new ArgumentOutOfRangeException();
+		if (flags == null)
+			throw new ArgumentNullException();
 
-		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), value);
+		TEnum multiEnum = flags[0];
+		for (int i = 1; i < flags.Length; i++)
+			multiEnum.Add(flags[i]);
+		return multiEnum;
 	}
 
 	/// <summary>
-	/// get an array of the individual flags
+	/// Get an array of the individual flags.
 	/// </summary>
 	public static TEnum[] GetArray<TEnum>(this TEnum multiEnum)where TEnum : struct, IConvertible {
 		int enumSize = Enum.GetValues(typeof(TEnum)).Length;
@@ -48,34 +73,65 @@ public static class MultiEnumHelper {
 	}
 
 	/// <summary>
-	/// gets the number of flags that r currently set
+	/// Gets the number of flags that are currently set.
 	/// </summary>
-	public static int GetNumFlags<TEnum>(this TEnum multiEnum)where TEnum : struct, IConvertible {
-		int enumSize = Enum.GetValues(typeof(TEnum)).Length;
+	public static int Count<TEnum>(this TEnum multiEnum)where TEnum : struct, IConvertible {
 		int bits = Convert.ToInt32(multiEnum);
-
 		int count = 0;
-		for (int i = 0; i < enumSize; i++)
+		for (int i = 0; i < 32; i++)
 			if ((bits & (1 << i)) > 0)
 				count++;
-
 		return count;
 	}
 
+	/// <summary>
+	/// Add a flag to the enum.
+	/// </summary>
 	public static void Add<TEnum>(ref this TEnum multiEnum, TEnum flag)where TEnum : struct, IConvertible {
 		int result = Convert.ToInt32(multiEnum) | Convert.ToInt32(flag);
 		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), result);
 	}
 
+	/// <summary>
+	/// Sets all the flags.
+	/// </summary>
+	public static void AddAll<TEnum>(ref this TEnum multiEnum)where TEnum : struct, IConvertible {
+		int value = 0;
+		int enumSize = Enum.GetValues(typeof(TEnum)).Length;
+		for (int i = 0; i < enumSize; i++)
+			value += 1 << i;
+
+		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), value);
+	}
+
+	/// <summary>
+	/// Removes the flag on the enum if it has it.
+	/// </summary>
 	public static void Remove<TEnum>(ref this TEnum multiEnum, TEnum flag)where TEnum : struct, IConvertible {
 		int result = Convert.ToInt32(multiEnum) & ~Convert.ToInt32(flag);
 		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), result);
 	}
 
+	/// <summary>
+	/// Removes all the flags on the enum.
+	/// </summary>
 	public static void RemoveAll<TEnum>(ref this TEnum multiEnum)where TEnum : struct, IConvertible {
 		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), 0);
 	}
 
+	/// <summary>
+	/// Either sets or removes all the flags on the enum.
+	/// </summary>
+	public static void SetAll<TEnum>(ref this TEnum multiEnum, bool on)where TEnum : struct, IConvertible {
+		if (on)
+			multiEnum.AddAll();
+		else
+			multiEnum.RemoveAll();
+	}
+
+	/// <summary>
+	/// Flip a flag on the enum.
+	/// </summary>
 	public static void Toggle<TEnum>(ref this TEnum multiEnum, TEnum flag)where TEnum : struct, IConvertible {
 		int result = Convert.ToInt32(multiEnum) & Convert.ToInt32(flag);
 		multiEnum = (TEnum)Enum.ToObject(typeof(TEnum), result);
