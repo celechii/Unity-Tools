cea79008758df5399fb336c96300590847d5de73
Noé <kathryncharron@cmail.carleton.ca>
2021-01-04 00:09:42 -0500

    new save system from dreamrealm kingdom

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
 
@@ -25,7 +26,9 @@ public static class SaveSystem {
 	
 	*/
 
+	public static int currentSaveSlot = 1;
 	private static string path;
+	private static string saveFileName = $"Save {currentSaveSlot}";
 
 	private static SaveData data;
 	public static SaveData Data {
@@ -39,7 +42,7 @@ public static class SaveSystem {
 	public static List<ICanBeSaved> savables = new List<ICanBeSaved>();
 
 	private static void SetPath() {
-		path = Application.persistentDataPath + "/Saves";
+		path = Path.Combine(Application.persistentDataPath, "Saves");
 		if (!Directory.Exists(path))
 			Directory.CreateDirectory(path);
 	}
@@ -55,21 +58,25 @@ public static class SaveSystem {
 	/// <summary>
 	/// Checks if the save exists.
 	/// </summary>
-	/// <param name="fileName">The file name, INCLUDING the extension, even for txts.</param>
+	/// <param name="fileName">The file name, INCLUDING the extension, even for txts.
+	/// Include '#' to be replaced with the current save slot.</param>
 	public static bool SaveExists(string fileName) {
+		fileName = fileName.Replace("#", currentSaveSlot.ToString());
 		SetPath();
-		return File.Exists(path + "/" + fileName);
+		return File.Exists(Path.Combine(path, fileName));
 	}
 
 	/// <summary>
 	/// Saves to text file in the resources folder.
 	/// </summary>
-	/// <param name="fileName">File name, not including the extension. Saves as a .txt</param>
+	/// <param name="fileName">File name, not including the extension.
+	/// Include '#' to be replaced with the current save slot.</param>
 	/// <param name="data">JSON data string.</param>
 	public static void SaveTxt(string fileName, object data) {
+		fileName = fileName.Replace("#", currentSaveSlot.ToString());
 		SetPath();
 		string stringData = JsonUtility.ToJson(data, true);
-		StreamWriter writer = new StreamWriter(path + "/" + fileName + ".txt");
+		StreamWriter writer = new StreamWriter(Path.Combine(path, fileName + ".txt"));
 		writer.Write(stringData);
 		writer.Close();
 	}
@@ -78,47 +85,96 @@ public static class SaveSystem {
 	/// Loads a text from text file.
 	/// </summary>
 	/// <returns>The from text file.</returns>
-	/// <param name="fileName">File name excluding the extention.</param>
+	/// <param name="fileName">File name excluding the extention.
+	/// Include '#' to be replaced with the current save slot.</param>
 	public static T LoadTxt<T>(string fileName) {
+		fileName = fileName.Replace("#", currentSaveSlot.ToString());
 		SetPath();
-		StreamReader reader = new StreamReader(path + "/" + fileName + ".txt");
+		StreamReader reader = new StreamReader(Path.Combine(path, fileName + ".txt"));
 		string stringData = reader.ReadToEnd();
 		reader.Close();
 		return JsonUtility.FromJson<T>(stringData);
 	}
 
 	#if UNITY_EDITOR
-	[UnityEditor.MenuItem("Save Shit/Load Game")]
+	[UnityEditor.MenuItem("Save Shit/Open/Current Save Game")]
+	private static void OpenSave() => OpenFile("Save #.txt");
+
+	public static void OpenFile(string fileName) {
+		fileName = fileName.Replace("#", currentSaveSlot.ToString());
+		SetPath();
+		string fullPath = Path.Combine(path, fileName);
+		if (File.Exists(fullPath))
+			Process.Start(fullPath);
+		else
+			throw new System.Exception($"wtf {fullPath} isnt a thing???");
+	}
+	#endif
+
+	#if UNITY_EDITOR
+	[UnityEditor.MenuItem("Save Shit/Load/Game Save")]
 	#endif
 	public static void Load() {
-		if (SaveExists("SaveData.txt")) {
-			data = LoadTxt<SaveData>("SaveData");
+		bool validSaveFound = false;
+
+		if (SaveExists(saveFileName + ".txt")) {
+			data = LoadTxt<SaveData>(saveFileName);
 			if (data == null)
 				data = new SaveData();
 			else
-				foreach (ICanBeSaved s in savables)
-					s.Load();
+				validSaveFound = true;
 		} else
 			data = new SaveData();
+
+		foreach (ICanBeSaved s in savables) {
+			if (validSaveFound)
+				s.Load();
+			else
+				s.Initialize();
+		}
 	}
 
 	#if UNITY_EDITOR
-	[UnityEditor.MenuItem("Save Shit/Save Game")]
+	[UnityEditor.MenuItem("Save Shit/Save/Game Save")]
 	#endif
 	public static void Save() {
 		foreach (ICanBeSaved s in savables)
 			s.Save();
-		SaveTxt("SaveData", data);
+		SaveTxt(saveFileName, data);
+	}
+
+	#if UNITY_EDITOR
+	[UnityEditor.MenuItem("Save Shit/DANGER/DELETE ALL SAVE FILES")]
+	private static void DeleteAllSaveFiles() {
+		SetPath();
+		foreach (string s in Directory.GetFiles(path)) {
+			File.Delete(s);
+		}
+	}
+	#endif
+
+	/// <summary>
+	/// Gets the names of the files saved in the save folder.
+	/// </summary>
+	public static string[] GetSaveNames() {
+		SetPath();
+		string[] files = Directory.GetFiles(path);
+		for (int i = 0; i < files.Length; i++)
+			files[i] = Path.GetFileNameWithoutExtension(files[i]);
+		return files;
 	}
 
 	[System.Serializable]
 	public class SaveData {
-		// save fields
+		public float randomSaveValue = Random.value;
+		public YarnVariables.YarnVariable[] yarnVariables = new YarnVariables.YarnVariable[0];
+		public Vector2 playerPos;
+		public int[] playerCards;
 	}
 }
 
 public interface ICanBeSaved {
-	void SubscribeToSaveSystem();
 	void Save();
 	void Load();
+	void Initialize();
 }
